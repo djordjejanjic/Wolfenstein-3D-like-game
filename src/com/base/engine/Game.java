275066c5f0e29bde35cde202f48d3338aefbc3b6
djord <djord@DESKTOP-EA378QQ>
2020-08-08 16:56:57 +0200

    Testiranje ucitavanja texture

--- src/com/base/engine/Game.java
+++ src/com/base/engine/Game.java
@@ -3,6 +3,11 @@ package com.base.engine;
 public class Game 
 {
 	Bitmap level;
+	Shader shader;
+	Texture texture;
+	Mesh mesh;
+	Material material;
+	Transform transform;
 	
 	public Game()
 	{
@@ -15,21 +20,39 @@ public class Game
 			}
 			System.out.println();
 		}
+		
+		shader = BasicShader.getInstance();
+		material = new Material(new Texture("WolfCollection.png"));
+		
+		Vertex[] vertices = new Vertex[] {new Vertex(new Vector3f(0, 0, 0),new Vector2f(0,0)),
+				new Vertex(new Vector3f(0, 1, 0),new Vector2f(0,1)),
+				new Vertex(new Vector3f(1, 1, 0),new Vector2f(1,1)),
+				new Vertex(new Vector3f(1, 0, 0),new Vector2f(1,0))};
+		
+		int[] indices = new int[] {0, 1, 2,
+								   0, 2, 3};
+		
+		mesh = new Mesh(vertices, indices);
+		transform = new Transform();
+		Transform.setCamera(new Camera());
+		Transform.setProjection(70, Window.getWidth(), Window.getHeight(), 0.01f, 1000f);
 	}
 	
 	public void input()
 	{
-
+		Transform.getCamera().input();
 	}
 	
 	public void update()
 	{
-
+		
 	}
 	
 	public void render()
 	{
-;
+		shader.bind();
+		shader.updateUniforms(transform.getTransformation(), transform.getProjectedTransformation(), material);
+		mesh.draw();
 	}
 
 }
